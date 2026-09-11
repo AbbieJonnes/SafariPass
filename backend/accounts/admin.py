c26ba5be5b3bd90305ff6422405b75cd638adb70
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-12 01:26:07 +0300

    feat: ensure NotificationLog model is registered in admin panel

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from .models import NotificationLog
 
 admin.site.register(User, UserAdmin)
+
+admin.site.register(NotificationLog)
