587bf114fb1aaf7adee99c2314645ab02a5531ae
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-11 23:54:37 +0300

    feat: register Company, Route, Fare, and PlanType models in admin panel

--- backend/companies/admin.py
+++ backend/companies/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Company, Route, Fare, PlanType
 
-# Register your models here.
+admin.site.register(Company)
+admin.site.register(Route)
+admin.site.register(Fare)
+admin.site.register(PlanType)
