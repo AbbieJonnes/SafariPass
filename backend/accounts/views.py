22d73f99e74103b6ebccd360b0cdb4326d8508da
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-12 16:03:02 +0300

    feat: implement User and NotificationLog list/create views

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import User, NotificationLog
+from .serializers import UserSerializer, NotificationLogSerializer
 
-# Create your views here.
+
+class UserListCreateView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class NotificationLogListCreateView(generics.ListCreateAPIView):
+    queryset = NotificationLog.objects.all()
+    serializer_class = NotificationLogSerializer
