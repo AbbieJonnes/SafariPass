468bdb784c71a8aa6196da3e0403015bc4851d60
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-12 12:44:01 +0300

    feat: add RouteSerializer for Route model in serializers

--- backend/companies/serializers.py
+++ backend/companies/serializers.py
@@ -1,8 +1,13 @@
 from rest_framework import serializers
-from .models import Company
+from .models import Company, Route
 
 
 class CompanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Company
         fields = '__all__'
+
+class RouteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Route
+        fields = '__all__' 
