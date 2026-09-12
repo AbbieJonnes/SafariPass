3d1661d0bae306e35f9295acdfadbff875844b80
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-12 09:57:50 +0300

    refactor: consolidate import statements in admin.py

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
-from .models import NotificationLog
+from .models import User, NotificationLog
 
 admin.site.register(User, UserAdmin)
 
