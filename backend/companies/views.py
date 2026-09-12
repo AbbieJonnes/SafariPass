2769238ef460943d0b0a5c0c19c81d94399204c6
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-12 12:44:11 +0300

    feat: add Route model and serializer to views for RouteListCreateView

--- backend/companies/views.py
+++ backend/companies/views.py
@@ -1,8 +1,12 @@
 from rest_framework import generics
-from .models import Company
-from .serializers import CompanySerializer
+from .models import Company, Route
+from .serializers import CompanySerializer, RouteSerializer
 
 
 class CompanyListCreateView(generics.ListCreateAPIView):
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
+
+class RouteListCreateView(generics.ListCreateAPIView):
+    queryset = Route.objects.all()
+    serializer_class = RouteSerializer
