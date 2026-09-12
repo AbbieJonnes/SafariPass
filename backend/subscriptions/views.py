11e2c854c540bf4c88cd182894a8add02dc6c5ed
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-12 15:50:31 +0300

    feat: add RouteShiftListCreateView for managing route shifts

--- backend/subscriptions/views.py
+++ backend/subscriptions/views.py
@@ -1,8 +1,12 @@
 from rest_framework import generics
-from .models import Subscription
-from .serializers import SubscriptionSerializer
+from .models import Subscription, RouteShift
+from .serializers import SubscriptionSerializer, RouteShiftSerializer
 
 
 class SubscriptionListCreateView(generics.ListCreateAPIView):
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
+
+class RouteShiftListCreateView(generics.ListCreateAPIView):
+    queryset = RouteShift.objects.all()
+    serializer_class = RouteShiftSerializer
