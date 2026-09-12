47af2be8cf9649af166e15c471640dd9cdd38ff1
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-12 15:56:06 +0300

    feat: add route shift URL pattern for managing route shifts

--- backend/subscriptions/urls.py
+++ backend/subscriptions/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SubscriptionListCreateView
+from .views import SubscriptionListCreateView, RouteShiftListCreateView
 
 urlpatterns = [
     path('', SubscriptionListCreateView.as_view(), name='subscription-list-create'),
+    path('shifts/', RouteShiftListCreateView.as_view(), name='route-shift-list-create'),
 ]
