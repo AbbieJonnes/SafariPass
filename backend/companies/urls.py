6d4a1bf62a13fd8f8b18c02872fc7c3feb2674a3
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-12 12:43:46 +0300

    feat: add route for RouteListCreateView in companies URL patterns

--- backend/companies/urls.py
+++ backend/companies/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CompanyListCreateView
+from .views import CompanyListCreateView, RouteListCreateView
 
 urlpatterns = [
     path('', CompanyListCreateView.as_view(), name='company-list-create'),
+    path('routes/', RouteListCreateView.as_view(), name='route-list-create'),
 ]
