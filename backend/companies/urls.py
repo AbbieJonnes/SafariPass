e5a9a14bb804089dcb901720c8e88c3378f9f77e
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-12 13:15:21 +0300

    feat: add URLs for Fare and PlanType views in companies URL patterns

--- backend/companies/urls.py
+++ backend/companies/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import CompanyListCreateView, RouteListCreateView
+from .views import (
+    CompanyListCreateView, 
+    RouteListCreateView, 
+    FareListCreateView, 
+    PlanTypeListCreateView)
 
 urlpatterns = [
     path('', CompanyListCreateView.as_view(), name='company-list-create'),
     path('routes/', RouteListCreateView.as_view(), name='route-list-create'),
+    path('fares/', FareListCreateView.as_view(), name='fare-list-create'),
+    path('plan-types/', PlanTypeListCreateView.as_view(), name='plan-type-list-create'),
 ]
