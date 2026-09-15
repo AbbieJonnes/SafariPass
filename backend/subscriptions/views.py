84c7a148eb813d74b599eca1553a6268e8c9ccdc
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-15 23:56:20 +0300

    feat: add custom permission checks for POST requests in Subscription and RouteShift views

--- backend/subscriptions/views.py
+++ backend/subscriptions/views.py
@@ -1,12 +1,27 @@
 from rest_framework import generics
 from .models import Subscription, RouteShift
 from .serializers import SubscriptionSerializer, RouteShiftSerializer
+from accounts.permissions import IsPassenger
 
 
 class SubscriptionListCreateView(generics.ListCreateAPIView):
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
 
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsPassenger()]
+        return super().get_permissions()
+
+    def perform_create(self, serializer):
+        serializer.save(passenger=self.request.user)
+
+
 class RouteShiftListCreateView(generics.ListCreateAPIView):
     queryset = RouteShift.objects.all()
     serializer_class = RouteShiftSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [IsPassenger()]
+        return super().get_permissions()
