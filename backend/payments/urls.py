dc5deb7255487247b83dcf3c60a8fa679a25c176
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-17 22:56:21 +0300

    feat: add M-Pesa payment initiation and callback URL patterns

--- backend/payments/urls.py
+++ backend/payments/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import PaymentListCreateView, ValidationRecordListCreateView
+from .views import (
+    PaymentListCreateView,
+    ValidationRecordListCreateView,
+    InitiateMpesaPaymentView,
+    MpesaCallbackView,
+)
 
 urlpatterns = [
     path('', PaymentListCreateView.as_view(), name='payment-list-create'),
     path('validations/', ValidationRecordListCreateView.as_view(), name='validation-list-create'),
+    path('mpesa/initiate/', InitiateMpesaPaymentView.as_view(), name='mpesa-initiate'),
+    path('mpesa-callback/', MpesaCallbackView.as_view(), name='mpesa-callback'),
 ]
