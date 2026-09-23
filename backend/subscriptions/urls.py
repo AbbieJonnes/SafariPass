2aa504729429dca255f7a15d09496ac8b213a278
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-23 11:25:59 +0300

    feat: add URL route for daily checks in subscriptions

--- backend/subscriptions/urls.py
+++ backend/subscriptions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SubscriptionListCreateView, RouteShiftListCreateView
+from .views import SubscriptionListCreateView, RouteShiftListCreateView, RunDailyChecksView
 
 urlpatterns = [
     path('', SubscriptionListCreateView.as_view(), name='subscription-list-create'),
     path('shifts/', RouteShiftListCreateView.as_view(), name='route-shift-list-create'),
+    path('run-daily-checks/', RunDailyChecksView.as_view(), name='run-daily-checks'),
 ]
