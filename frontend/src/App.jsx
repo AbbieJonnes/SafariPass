b68ba0378484e15fd2e5635a1e691ea230f3a185
AbbieJonnes <abigaelmwangi534@gmail.com>
2026-09-26 02:44:34 +0300

    feat: update routing to remove redirect from landing to login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<Landing />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
-      <Route path="/" element={<Navigate to="/login" />} />
     </Routes>
   );
 }
